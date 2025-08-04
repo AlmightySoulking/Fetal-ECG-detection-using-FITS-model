832b31f05e6045b1c4a81d314a8f9b7bc387cd56
Sharad Kumar Mishra <sharadmishra1705@gmail.com>
2025-08-04 18:08:58 +0530

    Add files via upload

---
+++ exp/exp_basic.py
@@ -0,0 +1,40 @@
+import os
+import torch
+import numpy as np
+
+"""
+Defines the essential structure that any "experiment" in the given project must follow. It's like a blue print design that says any model
+created must know how to build a model, acquire a GPU/CPU, have methods for testing, training and validation. 
+"""
+class Exp_Basic(object):
+    def __init__(self, args):
+        self.args = args
+        self.device = self._acquire_device()
+        self.model = self._build_model().to(self.device)
+
+    def _build_model(self):
+        raise NotImplementedError
+        return None
+
+    def _acquire_device(self):
+        if self.args.use_gpu:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(
+                self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
+            device = torch.device('cuda:{}'.format(self.args.gpu))
+            print('Use GPU: cuda:{}'.format(self.args.gpu))
+        else:
+            device = torch.device('cpu')
+            print('Use CPU')
+        return device
+
+    def _get_data(self):
+        pass
+
+    def vali(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
